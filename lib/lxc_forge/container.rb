4f9bf2b1978ad8931b7a6510841bf428cfb4982e
Joshua B. Bussdieker <jbussdieker@gmail.com>
2014-04-09 22:13:31 -0700

    Such hack very dev

--- lib/lxc_forge/container.rb
+++ lib/lxc_forge/container.rb
@@ -5,5 +5,37 @@ module LxcForge
     def initialize(name)
       @name = name
     end
+
+    def compress
+      require 'zlib'
+      require 'archive/tar/minitar' 
+      Dir.chdir "/var/lib/lxc"
+      File.open("/tmp/#{@name}.tar", 'wb') do |f|
+        tar = Archive::Tar::Minitar::Output.new(f)
+        list = Find.find(@name)
+        #list.count
+        list.each do |entry|
+          puts entry
+          unless entry =~ /\/rootfs\/dev\/core$/
+            begin
+              Archive::Tar::Minitar.pack_file(entry, tar)
+            rescue Exception => e
+              puts e.message
+            end
+          end
+        end
+      end
+    end
+
+    def upload
+      require 'aws-sdk'
+      s3 = AWS::S3.new(
+        :access_key_id => '',
+        :secret_access_key => '')
+
+      bucket = s3.buckets[""]
+      obj = bucket.objects[@name]
+      obj.write(Pathname.new("/tmp/#{@name}.tar"))
+    end
   end
 end
