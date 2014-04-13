77a8d3fbff15a621ace25202ae0137967fa3ba0e
Joshua B. Bussdieker <jbussdieker@gmail.com>
2014-04-13 10:07:55 -0700

    Add a config class

--- lib/lxc_forge.rb
+++ lib/lxc_forge.rb
@@ -1,4 +1,5 @@
 require "lxc_forge/version"
+require "lxc_forge/config"
 require "lxc_forge/container"
 require "lxc_forge/command"
 require "lxc_forge/options"
---
+++ lib/lxc_forge/config.rb
@@ -0,0 +1,5 @@
+module LxcForge
+  class Config
+    attr_accessor :access_key_id, :secret_access_key, :region, :bucket
+  end
+end
